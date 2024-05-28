67ed97b88b6c9e2523fca84bc418b5c137429755
selja <seljaworks@gmail.com>
2024-05-28 12:31:38 +0700

    fix styling and reorder project grid by latest

--- src/Components/GridView/GridView.tsx
+++ src/Components/GridView/GridView.tsx
@@ -10,64 +10,69 @@ const GridView: React.FC<GridViewProps> = () => {
   return (
     <Flipper flipKey={projects.join("")}>
       <div className={styles["grid-view"]}>
-        {projects.map((project) => (
-          <Flipped key={project.id} flipId={project.id}>
-            <div key={project.id} className={styles["grid-item"]}>
-              <div className={styles.overlay}>
-                {project.githubLink && (
-                  <a
-                    title="Go to the source code and its description"
-                    target="_blank"
-                    href={project.githubLink}
-                    className={styles["contact-link"]}
-                  >
-                    <img
-                      src={
-                        new URL("../../img/github.svg", import.meta.url).href
-                      }
-                      alt={project.name}
-                    />
-                  </a>
-                )}
+        {projects
+          .map((project) => (
+            <Flipped key={project.id} flipId={project.id}>
+              <div key={project.id} className={styles["grid-item"]}>
+                <div className={styles.overlay}>
+                  {project.githubLink && (
+                    <a
+                      title="Go to the source code and its description"
+                      target="_blank"
+                      href={project.githubLink}
+                      className={styles["contact-link"]}
+                    >
+                      <img
+                        src={
+                          new URL("../../img/github.svg", import.meta.url).href
+                        }
+                        alt={project.name}
+                      />
+                    </a>
+                  )}
 
-                {project.liveUrl && (
-                  <a
-                    title="Go to the live demo"
-                    target="_blank"
-                    href={project.liveUrl}
-                    className={styles["contact-link"]}
-                  >
-                    <img
-                      src={new URL("../../img/globe.svg", import.meta.url).href}
-                      alt={project.name}
-                    />
-                  </a>
-                )}
-                {project.playStore && (
-                  <a
-                    title="Go to Play Store"
-                    target="_blank"
-                    href={project.playStore}
-                    className={styles["contact-link"]}
-                  >
-                    <img
-                      src={
-                        new URL("../../img/playstore.svg", import.meta.url).href
-                      }
-                      alt={project.name}
-                    />
-                  </a>
-                )}
+                  {project.liveUrl && (
+                    <a
+                      title="Go to the live demo"
+                      target="_blank"
+                      href={project.liveUrl}
+                      className={styles["contact-link"]}
+                    >
+                      <img
+                        src={
+                          new URL("../../img/globe.svg", import.meta.url).href
+                        }
+                        alt={project.name}
+                      />
+                    </a>
+                  )}
+                  {project.playStore && (
+                    <a
+                      title="Go to Play Store"
+                      target="_blank"
+                      href={project.playStore}
+                      className={styles["contact-link"]}
+                    >
+                      <img
+                        src={
+                          new URL("../../img/playstore.svg", import.meta.url)
+                            .href
+                        }
+                        alt={project.name}
+                      />
+                    </a>
+                  )}
+                </div>
+
+                <img
+                  className={styles.img}
+                  src={project.imgUrl}
+                  alt={`${project.category} project`}
+                />
               </div>
-
-              <img
-                className={styles.img}
-                src={project.imgUrl}
-                alt={`${project.category} project`}
-              />
-            </div>
-          </Flipped>
-        ))}
+            </Flipped>
+          ))
+          .reverse()}
       </div>
     </Flipper>
   );
